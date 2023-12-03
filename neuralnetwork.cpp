74cefb04d7d4b42727948789ccf655c9257800b5
shunsuke-kawata <kwt20238120@gmail.com>
2023-12-04 01:03:59 +0900

    update コメントの削除

--- neuralnetwork.cpp
+++ neuralnetwork.cpp
@@ -141,11 +141,8 @@ int main()
 void make_test(void)
 {
 	int t, i, o, tn;
-
 	for (t = 0; t < TEST_NUMBER; t++){
-
 		tn = (int)(((double)rand() / (1.0 + RAND_MAX)) * OUTPUT_NUMBER);
-		
 		for (i = 0; i < INPUT_NUMBER; i++){
 			if (((double)rand() / ((double)RAND_MAX + 1)) < 0.1){
 				test_input[t][i] = 1 - training_input[tn][i];
@@ -154,7 +151,6 @@ void make_test(void)
 				test_input[t][i] = training_input[tn][i];
 			}
 		}
-
 		for (o = 0; o < OUTPUT_NUMBER; o++){
 			test_output[t][o] = training_output[tn][o];
 		}
@@ -164,15 +160,11 @@ void make_test(void)
 void init_weight(void){
 
 	int h, i, o;
-
-	// input_weight�̏�����
 	for (h = 0; h < HIDDEN_NUMBER; h++){
 		for (i = 0; i < INPUT_NUMBER; i++){
 			input_weight[h][i] = ((double)rand() / ((double)RAND_MAX + 1)) * 0.2 - 0.1;
 		}
 	}
-
-	// hidden_weight�̏�����
 	for (o = 0; o < OUTPUT_NUMBER; o++){
 		for (h = 0; h < HIDDEN_NUMBER; h++){
 			hidden_weight[o][h] = ((double)rand() / ((double)RAND_MAX + 1)) * 0.2 - 0.1;
@@ -183,7 +175,6 @@ void init_weight(void){
 void learn_rate(ofstream& file,float learningLate){
 
 	int l, t, h, i, o;
-
 	double error,test_error;
 	double wix, whs;
 	double sigmoid_wix[TRAINING_NUMBER][HIDDEN_NUMBER];
@@ -193,9 +184,6 @@ void learn_rate(ofstream& file,float learningLate){
 	double delta_hidden_weight[OUTPUT_NUMBER][HIDDEN_NUMBER];
 
 	for (l = 0; l < LEARNING_TIME; l++){
-
-		// ���ԍזE�̏o�� sigmoid_wix �̌v�Z
-		// sigmoid�֐��̑���Ɂusigmoid�֐�����������֐��v���g�p�����ۂ̒��ԍזE�̏o�� delta_sigmoid_wix �̌v�Z
 		for (t = 0; t < TRAINING_NUMBER; t++){
 			for (h = 0; h < HIDDEN_NUMBER; h++){
 				wix = 0;
@@ -206,9 +194,6 @@ void learn_rate(ofstream& file,float learningLate){
 				delta_sigmoid_wix[t][h] = sigmoid_wix[t][h] * (1 - sigmoid_wix[t][h]);
 			}
 		}
-
-		// �o�͍זE�̏o�� whs �̌v�Z
-		// ���t�M�� - �o�͍זE o_whs �̌v�Z
 		for (t = 0; t < TRAINING_NUMBER; t++){
 			for (o = 0; o < OUTPUT_NUMBER; o++){
 				whs = 0;
@@ -218,9 +203,6 @@ void learn_rate(ofstream& file,float learningLate){
 				o_whs[t][o] = training_output[t][o] - whs;
 			}
 		}
-
-		// �P���f�[�^�̂Q��덷 error �̌v�Z�E�\��
-		// �e�X�g�f�[�^�̂Q��덷 test_error �̌v�Z�E�\��
 		error = 0;
 		for (t = 0; t < TRAINING_NUMBER; t++){
 			for (o = 0; o < OUTPUT_NUMBER; o++){
@@ -228,11 +210,8 @@ void learn_rate(ofstream& file,float learningLate){
 			}
 		}
 		test_error = get_test_error();
-		//学習ステップ毎の訓練データの２乗誤差（左）とテストデータの２乗誤差（右）を表示
-		// file << fixed << setprecision(5) << error << " " << test_error << endl;
 		file << fixed << setprecision(5)<<l+1<<" "<< test_error << endl;
 
-		// input_weight �ɂ�� E �̔����l�̌v�Z
 		for (h = 0; h < HIDDEN_NUMBER; h++){
 			for (i = 0; i < INPUT_NUMBER; i++){
 				delta_input_weight[h][i] = 0;
@@ -244,7 +223,6 @@ void learn_rate(ofstream& file,float learningLate){
 			}
 		}
 
-		// hidden_weight �ɂ�� E �̔����l�̌v�Z
 		for (o = 0; o < OUTPUT_NUMBER; o++){
 			for (h = 0; h < HIDDEN_NUMBER; h++){
 				delta_hidden_weight[o][h] = 0;
@@ -254,14 +232,12 @@ void learn_rate(ofstream& file,float learningLate){
 			}
 		}
 
-		// input_weight �̍X�V
 		for (h = 0; h < HIDDEN_NUMBER; h++){
 			for (i = 0; i < INPUT_NUMBER; i++){
 				input_weight[h][i] -= learningLate * delta_input_weight[h][i];
 			}
 		}
 
-		// hidden_weight �̍X�V
 		for (o = 0; o < OUTPUT_NUMBER; o++){
 			for (h = 0; h < HIDDEN_NUMBER; h++){
 				hidden_weight[o][h] -= learningLate * delta_hidden_weight[o][h];
@@ -273,7 +249,6 @@ void learn_rate(ofstream& file,float learningLate){
 void learn_hidden(ofstream& file,int hiddenNumber){
 
 	int l, t, h, i, o;
-
 	double error,test_error;
 	double wix, whs;
 	double sigmoid_wix[TRAINING_NUMBER][hiddenNumber];
@@ -283,9 +258,6 @@ void learn_hidden(ofstream& file,int hiddenNumber){
 	double delta_hidden_weight[OUTPUT_NUMBER][hiddenNumber];
 
 	for (l = 0; l < LEARNING_TIME; l++){
-
-		// ���ԍזE�̏o�� sigmoid_wix �̌v�Z
-		// sigmoid�֐��̑���Ɂusigmoid�֐�����������֐��v���g�p�����ۂ̒��ԍזE�̏o�� delta_sigmoid_wix �̌v�Z
 		for (t = 0; t < TRAINING_NUMBER; t++){
 			for (h = 0; h < hiddenNumber; h++){
 				wix = 0;
@@ -296,9 +268,6 @@ void learn_hidden(ofstream& file,int hiddenNumber){
 				delta_sigmoid_wix[t][h] = sigmoid_wix[t][h] * (1 - sigmoid_wix[t][h]);
 			}
 		}
-
-		// �o�͍זE�̏o�� whs �̌v�Z
-		// ���t�M�� - �o�͍זE o_whs �̌v�Z
 		for (t = 0; t < TRAINING_NUMBER; t++){
 			for (o = 0; o < OUTPUT_NUMBER; o++){
 				whs = 0;
@@ -308,9 +277,6 @@ void learn_hidden(ofstream& file,int hiddenNumber){
 				o_whs[t][o] = training_output[t][o] - whs;
 			}
 		}
-
-		// �P���f�[�^�̂Q��덷 error �̌v�Z�E�\��
-		// �e�X�g�f�[�^�̂Q��덷 test_error �̌v�Z�E�\��
 		error = 0;
 		for (t = 0; t < TRAINING_NUMBER; t++){
 			for (o = 0; o < OUTPUT_NUMBER; o++){
@@ -318,13 +284,10 @@ void learn_hidden(ofstream& file,int hiddenNumber){
 			}
 		}
 		test_error = get_test_error();
-		//学習ステップ毎の訓練データの２乗誤差（左）とテストデータの２乗誤差（右）を表示
-		// file << fixed << setprecision(5) << error << " " << test_error << endl;
 		if(l==LEARNING_TIME-1){
 			file << fixed << setprecision(5)<<hiddenNumber<<" "<< test_error << endl;
 		}
 
-		// input_weight �ɂ�� E �̔����l�̌v�Z
 		for (h = 0; h < hiddenNumber; h++){
 			for (i = 0; i < INPUT_NUMBER; i++){
 				delta_input_weight[h][i] = 0;
@@ -336,7 +299,6 @@ void learn_hidden(ofstream& file,int hiddenNumber){
 			}
 		}
 
-		// hidden_weight �ɂ�� E �̔����l�̌v�Z
 		for (o = 0; o < OUTPUT_NUMBER; o++){
 			for (h = 0; h < hiddenNumber; h++){
 				delta_hidden_weight[o][h] = 0;
@@ -346,14 +308,12 @@ void learn_hidden(ofstream& file,int hiddenNumber){
 			}
 		}
 
-		// input_weight �̍X�V
 		for (h = 0; h < hiddenNumber; h++){
 			for (i = 0; i < INPUT_NUMBER; i++){
 				input_weight[h][i] -= LEARNING_RATE * delta_input_weight[h][i];
 			}
 		}
 
-		// hidden_weight �̍X�V
 		for (o = 0; o < OUTPUT_NUMBER; o++){
 			for (h = 0; h < hiddenNumber; h++){
 				hidden_weight[o][h] -= LEARNING_RATE * delta_hidden_weight[o][h];
@@ -374,9 +334,6 @@ float learn_all(int hiddenNumber,float learningLate,int learningTime){
 	double delta_hidden_weight[OUTPUT_NUMBER][hiddenNumber];
 
 	for (l = 0; l < learningTime; l++){
-
-		// ���ԍזE�̏o�� sigmoid_wix �̌v�Z
-		// sigmoid�֐��̑���Ɂusigmoid�֐�����������֐��v���g�p�����ۂ̒��ԍזE�̏o�� delta_sigmoid_wix �̌v�Z
 		for (t = 0; t < TRAINING_NUMBER; t++){
 			for (h = 0; h < hiddenNumber; h++){
 				wix = 0;
@@ -387,9 +344,6 @@ float learn_all(int hiddenNumber,float learningLate,int learningTime){
 				delta_sigmoid_wix[t][h] = sigmoid_wix[t][h] * (1 - sigmoid_wix[t][h]);
 			}
 		}
-
-		// �o�͍זE�̏o�� whs �̌v�Z
-		// ���t�M�� - �o�͍זE o_whs �̌v�Z
 		for (t = 0; t < TRAINING_NUMBER; t++){
 			for (o = 0; o < OUTPUT_NUMBER; o++){
 				whs = 0;
@@ -399,9 +353,6 @@ float learn_all(int hiddenNumber,float learningLate,int learningTime){
 				o_whs[t][o] = training_output[t][o] - whs;
 			}
 		}
-
-		// �P���f�[�^�̂Q��덷 error �̌v�Z�E�\��
-		// �e�X�g�f�[�^�̂Q��덷 test_error �̌v�Z�E�\��
 		error = 0;
 		for (t = 0; t < TRAINING_NUMBER; t++){
 			for (o = 0; o < OUTPUT_NUMBER; o++){
@@ -409,8 +360,6 @@ float learn_all(int hiddenNumber,float learningLate,int learningTime){
 			}
 		}
 		test_error = get_test_error();
-
-		// input_weight �ɂ�� E �̔����l�̌v�Z
 		for (h = 0; h < hiddenNumber; h++){
 			for (i = 0; i < INPUT_NUMBER; i++){
 				delta_input_weight[h][i] = 0;
@@ -421,8 +370,6 @@ float learn_all(int hiddenNumber,float learningLate,int learningTime){
 				}
 			}
 		}
-
-		// hidden_weight �ɂ�� E �̔����l�̌v�Z
 		for (o = 0; o < OUTPUT_NUMBER; o++){
 			for (h = 0; h < hiddenNumber; h++){
 				delta_hidden_weight[o][h] = 0;
@@ -431,15 +378,11 @@ float learn_all(int hiddenNumber,float learningLate,int learningTime){
 				}
 			}
 		}
-
-		// input_weight �̍X�V
 		for (h = 0; h < hiddenNumber; h++){
 			for (i = 0; i < INPUT_NUMBER; i++){
 				input_weight[h][i] -= learningLate * delta_input_weight[h][i];
 			}
 		}
-
-		// hidden_weight �̍X�V
 		for (o = 0; o < OUTPUT_NUMBER; o++){
 			for (h = 0; h < hiddenNumber; h++){
 				hidden_weight[o][h] -= learningLate * delta_hidden_weight[o][h];
